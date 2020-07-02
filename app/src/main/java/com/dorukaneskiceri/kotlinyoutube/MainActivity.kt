80dc4bb273480ffe66041cf30d6be1734294184a
DorukanE <dorukaneskiceri@gmail.com>
2020-07-02 14:17:23 +0300

    application screenshots added

--- app/src/main/java/com/dorukaneskiceri/kotlinyoutube/MainActivity.kt
+++ app/src/main/java/com/dorukaneskiceri/kotlinyoutube/MainActivity.kt
@@ -3,17 +3,12 @@ package com.dorukaneskiceri.kotlinyoutube
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.CalendarContract
-import android.provider.MediaStore
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
